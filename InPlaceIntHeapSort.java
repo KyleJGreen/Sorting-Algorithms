97d3c06968c9a1893437af330fe4839035ba5299
Kyle J Green <greenk4@nku.edu>
2016-10-08 22:42:09 -0400

    Add files via upload

---
+++ InPlaceIntHeapSort.java
@@ -0,0 +1,72 @@
+/* Kyle Green
+ * CSC 360-001
+ * 
+ * A tree structure algorithm where the left branches of the trees are filled in first and no child
+ * branch can have a greater value than that of its parent. Swaps the root of the heap
+ * (the greatest element) with the end of the array, reorganizes the heap and repeats this
+ * process until the array is sorted.
+ */
+public class InPlaceIntHeapSort {
+
+	public static void heapSort(int[] array)
+	{
+		// Turn the array into a max-heap
+		for(int i = 0; i < array.length; i++)
+		{
+			// "Sift up" element at index i
+			int index = i;
+			while (index > 0)
+			{
+				int parentIndex = (index - 1) / 2;
+				int parent = array[parentIndex];
+				int element = array[index];
+
+				if(parent >= element)
+					break;
+
+				array[index] = parent;
+				array[parentIndex] = element;
+				index = parentIndex;
+			}
+		}
+
+//		 Repeatedly extract the max element from the heap
+		for(int i = array.length - 1; i > 0; i--)
+		{
+			// The BIG swap!
+			int temp = array[0];
+			array[0] = array[i];
+			array[i] = temp;
+			// Sift down element at index 0
+			int parentIndex = 0;
+			
+			// in while loop, check if the children are greater and check out of bounds against i
+			while(i > 1)
+			{
+				int childIndex;
+				//left child
+				int leftChildIndex = 2 * parentIndex + 1;
+				if(leftChildIndex >= i)
+					break;
+				
+				//right child
+				int rightChildIndex = 2 * parentIndex + 2;
+				
+				// assign child index
+				if (rightChildIndex >=i || array[leftChildIndex] >= array[rightChildIndex])
+					childIndex = leftChildIndex;
+				else
+					childIndex = rightChildIndex;
+				
+				int parent = array[parentIndex];
+				int child = array[childIndex];
+				
+				if(parent >= child)
+					break;
+				array[parentIndex] = child;
+				array[childIndex] = parent;
+				parentIndex = childIndex;
+			}
+		}
+	}
+}
---
+++ RadixSort.java
@@ -0,0 +1,47 @@
+/* Kyle Green
+ * CSC 360-001
+ * 
+ * For every iteration, n is multiplied by ten and the elements are reassigned
+ * to new buckets until this process has been done for ten digits, producing a sorted array.
+ */
+
+import java.util.ArrayList;
+
+public class RadixSort
+{
+	public static void radixSort(int[] array)
+	{
+		ArrayList<ArrayList<Integer>> buckets = new ArrayList<ArrayList<Integer>>();
+
+		// Create the buckets
+		for(int i = -9; i <= 9; i++)
+		{
+			ArrayList<Integer> bucket = new ArrayList<Integer>();
+			buckets.add(bucket);
+		}
+
+		int d = 1;
+		for(int j = d; j <= 10; j++)
+		{
+			// Bucketize
+			for(int i = 0; i < array.length; i++)
+			{
+				int k = array[i] / d % 10 + 9;
+				buckets.get(k).add(array[i]);
+			}
+			//put back into the array every iteration
+			int count = 0;
+			for(int i = 0; i < buckets.size(); i++)
+			{
+				int size = buckets.get(i).size();
+				for(int m = 0; m < size; m++)
+				{
+					array[count] = buckets.get(i).get(m);
+					count++;
+				}
+				buckets.get(i).clear();
+			}
+			d *= 10;
+		}
+	}
+}
